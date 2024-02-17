32ae3f8ea5969953571844bd74fde73110446128
SobakaBekka <st69704@upce.cz>
2024-02-17 21:51:52 +0100

    done the tasks

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        double result = toBeDivided / 5;
+        System.out.println(result);
+
     }
 }
